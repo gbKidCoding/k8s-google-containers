9edc71b220f7c77636b2163a33d46d3e7d63212f
majun <majun@kedacom.com>
2019-01-11 14:05:41 +0800

    修复docker版本问题

--- kubernates-centos7/kubernates/script/install_docker.sh
+++ kubernates-centos7/kubernates/script/install_docker.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-curl -sSL https://get.daocloud.io/docker | sh
+
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum -y install docker-ce-18.06.1.ce-3.el7
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://e7850958.m.daocloud.io
 systemctl enable docker
 systemctl restart docker
+service docker restart
