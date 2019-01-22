e78d0205ae3f99bd673764b6cdc342e405e4f09c
majun <majun@kedacom.com>
2019-01-22 14:21:04 +0800

    my sh

---
+++ my-script/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sh install_centos7_common.sh
+sh install_docker.sh
+sh install_docker_compose.sh
--- my-script/install_centos7_common.sh
+++ my-script/install_centos7_common.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+systemctl stop firewalld
+systemctl disable firewalld
+sed -i "s#SELINUX=enforcing#SELINUX=disabled#g" /etc/sysconfig/selinux
+
 sudo yum install -y vim
 sudo yum install -y git
 sudo yum install -y wget
 sudo yum install -y bash-completion
 
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+yum makecache
