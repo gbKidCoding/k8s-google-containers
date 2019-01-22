68ff230e1879ca0f9fc8aa84ef59313cd1d678f1
majun <majun@kedacom.com>
2019-01-22 14:16:06 +0800

    common sh install -y

--- my-script/install_centos7_common.sh
+++ my-script/install_centos7_common.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo yum install vim
-sudo yum install git
-sudo yum install wget
-sudo yum install bash-completion
+sudo yum install -y vim
+sudo yum install -y git
+sudo yum install -y wget
+sudo yum install -y bash-completion
 
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
