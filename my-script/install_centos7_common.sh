cc91e88913b8d9203a04503d738ffcebbb08c58d
majun <majun@kedacom.com>
2019-01-23 10:34:38 +0800

    common sh

--- my-script/install_centos7_common.sh
+++ my-script/install_centos7_common.sh
@@ -9,6 +9,10 @@ sudo yum install -y git
 sudo yum install -y wget
 sudo yum install -y bash-completion
 
+# use aliyun repo and download faster
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-
 yum makecache
+
+# sync date form ntp server pool
+sudo yum -y install ntpdate
+sudo ntpdate 0.cn.pool.ntp.org
